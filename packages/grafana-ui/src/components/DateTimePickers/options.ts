1c96fbc689b1a9e4abb80ba3fbf53d33ef8761d8
Gurinder Singh <gurinder@coderabbit.ai>
2025-04-25 12:15:50 -0400

    removed time range option under 24hrs

--- packages/grafana-ui/src/components/DateTimePickers/options.ts
+++ packages/grafana-ui/src/components/DateTimePickers/options.ts
@@ -1,13 +1,13 @@
 import { SelectableValue, TimeOption } from '@grafana/data';
 
 export const quickOptions: TimeOption[] = [
-  { from: 'now-5m', to: 'now', display: 'Last 5 minutes' },
-  { from: 'now-15m', to: 'now', display: 'Last 15 minutes' },
-  { from: 'now-30m', to: 'now', display: 'Last 30 minutes' },
-  { from: 'now-1h', to: 'now', display: 'Last 1 hour' },
-  { from: 'now-3h', to: 'now', display: 'Last 3 hours' },
-  { from: 'now-6h', to: 'now', display: 'Last 6 hours' },
-  { from: 'now-12h', to: 'now', display: 'Last 12 hours' },
+  // { from: 'now-5m', to: 'now', display: 'Last 5 minutes' },
+  // { from: 'now-15m', to: 'now', display: 'Last 15 minutes' },
+  // { from: 'now-30m', to: 'now', display: 'Last 30 minutes' },
+  // { from: 'now-1h', to: 'now', display: 'Last 1 hour' },
+  // { from: 'now-3h', to: 'now', display: 'Last 3 hours' },
+  // { from: 'now-6h', to: 'now', display: 'Last 6 hours' },
+  // { from: 'now-12h', to: 'now', display: 'Last 12 hours' },
   { from: 'now-24h', to: 'now', display: 'Last 24 hours' },
   { from: 'now-2d', to: 'now', display: 'Last 2 days' },
   { from: 'now-7d', to: 'now', display: 'Last 7 days' },
