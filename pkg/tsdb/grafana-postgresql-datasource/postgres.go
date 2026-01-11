3a4b37c28812f09465e0a77413804af6b47755aa
Gurinder Singh <gurinder@coderabbit.ai>
2026-01-11 10:35:14 -0500

    database is closed issue fix

--- pkg/tsdb/grafana-postgresql-datasource/postgres.go
+++ pkg/tsdb/grafana-postgresql-datasource/postgres.go
@@ -48,7 +48,7 @@ func ProvideService(cfg *setting.Cfg) *Service {
 			s.crDB = sql.OpenDB(connector)
 			s.crDB.SetMaxOpenConns(100)
 			s.crDB.SetMaxIdleConns(10)
-			s.crDB.SetConnMaxLifetime(time.Duration(14400) * time.Second)
+			s.crDB.SetConnMaxLifetime(0)
 
 			if err := s.crDB.Ping(); err != nil {
 				logger.Error("CodeRabbit postgres connection failed", "error", err)
